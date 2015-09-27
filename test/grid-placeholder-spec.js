31b691db40a270a9fb911b50713cbec093e84bbc
Ben Morton <ben.morton91@gmail.com>
2015-09-27 14:02:58 +0000

    Remove compiled test files and add to gitignore

--- test/grid-placeholder-spec.js
+++
@@ -1,9 +0,0 @@
-function main() {
-    describe("NgGridPlaceholder Component", function () {
-        it("should ", function () {
-        });
-    });
-}
-exports.main = main;
-
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImdyaWQtcGxhY2Vob2xkZXItc3BlYy50cyJdLCJuYW1lcyI6WyJtYWluIl0sIm1hcHBpbmdzIjoiQUFFQTtJQUNDQSxRQUFRQSxDQUFDQSw2QkFBNkJBLEVBQUVBO1FBQ3ZDQSxFQUFFQSxDQUFDQSxTQUFTQSxFQUFFQTtRQUNkQSxDQUFDQSxDQUFDQSxDQUFDQTtJQUNKQSxDQUFDQSxDQUFDQSxDQUFDQTtBQUNKQSxDQUFDQTtBQUxlLFlBQUksT0FLbkIsQ0FBQSIsImZpbGUiOiJncmlkLXBsYWNlaG9sZGVyLXNwZWMuanMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBOZ0dyaWQsIE5nR3JpZEl0ZW0sIE5nR3JpZFBsYWNlaG9sZGVyIH0gZnJvbSAnc3JjL05nR3JpZCc7XHJcblxyXG5leHBvcnQgZnVuY3Rpb24gbWFpbigpIHtcclxuXHRkZXNjcmliZShcIk5nR3JpZFBsYWNlaG9sZGVyIENvbXBvbmVudFwiLCAoKSA9PiB7XHJcblx0XHRpdChcInNob3VsZCBcIiwgKCkgPT4ge1xyXG5cdFx0fSk7XHJcblx0fSk7XHJcbn0iXSwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
