91952b24fbd0ae31a1c342eb2396e3f8d091da59
Ben Morton <ben.morton91@gmail.com>
2016-09-13 21:53:07 +0000

    Update changes to fix things

--- src/modules/NgGrid.module.ts
+++ src/modules/NgGrid.module.ts
@@ -1,10 +1,7 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgGrid, NgGridItem, NgGridItemConfig, NgGridItemEvent, NgGridPlaceholder } from '../main';
 
 @NgModule({
-  imports:          [ BrowserModule, CommonModule ],
   declarations:     [ NgGrid, NgGridItem, NgGridPlaceholder ],
   entryComponents:  [ NgGridPlaceholder ],
   exports:          [ NgGrid, NgGridItem ]
