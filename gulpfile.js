d39bbfdd66daf640f8f3268025350b0058be43c9
Ben Morton <ben.morton91@gmail.com>
2017-01-24 00:02:53 +0000

    Add bundles for grid

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,22 @@ var rename = require('gulp-rename');
 var exec = require('child_process').exec;
 var symlink = require('gulp-symlink');
 var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
 var runSequence = require('run-sequence');
 var merge = require('merge2');
+var rollup = require('rollup-stream');
+var tsRollup = require('rollup-plugin-typescript');
+var angularRollup = require("rollup-plugin-angular");
+var nodeResolve = require("rollup-plugin-node-resolve");
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 // var KarmaServer = require('karma').Server;
 
 var tsProject = typescript.createProject('tsconfig.json');
+var tsRollupProject = typescript.createProject('tsconfig.json', {
+	allowJs: true,
+	declaration: false
+});
 
 var PATHS = {
 	src: {
@@ -39,19 +50,83 @@ gulp.task('ngc', function(done) {
 	});
 });
 
+gulp.task('rollup-ngc', function(done) {
+	exec(path.join("node_modules", ".bin", "ngc") + ' -p tsconfig.aot.bundle.json', function (err, stdout, stderr) {
+		if (stdout) console.log(stdout);
+		if (stderr) console.error(stderr);
+		done(err);
+	});
+});
+
 gulp.task('ts', function () {
 	var tsResult = gulp.src(PATHS.src.ts)
 		.pipe(sourcemaps.init())
-		.pipe(tsProject());
+		.pipe(tsProject().on("error", function() { process.exit(1); }));
 
 	return merge([
 		tsResult.js.pipe(sourcemaps.write()).pipe(gulp.dest(path.join('dist', 'js'))),
-		tsResult.dts.pipe(gulp.dest(path.join('dist', 'dts')))
+		tsResult.dts.pipe(gulp.dest(path.join('dist', 'typings')))
 	]);
 });
 
+gulp.task("rollup", function() {
+	return rollup({
+		entry: "./src/main.ts",
+		external: [
+			"@angular/core",
+			"rxjs"
+		],
+		format: "cjs",
+		sourceMap: true,
+		plugins: [
+			tsRollup({
+				tsconfig: false,
+				target: "es5",
+				moduleResolution: "node",
+				sourceMap: true,
+				emitDecoratorMetadata: true,
+				experimentalDecorators: true,
+				noImplicitAny: false,
+			}),
+			angularRollup(),
+			nodeResolve({ jsnext: true, main: true })
+		]
+	})
+	.pipe(source("main.ts", "./src/"))
+	.pipe(buffer())
+	.pipe(rename('NgGrid.js'))
+	.pipe(gulp.dest('./dist/bundles'))
+	.pipe(uglify())
+	.pipe(rename('NgGrid.min.js'))
+	.pipe(gulp.dest('./dist/bundles'));
+});
+
+gulp.task("rollup-aot", ["rollup-ngc"], function() {
+	return rollup({
+		entry: "./temp/main.js",
+		external: [
+			"@angular/core",
+			"rxjs"
+		],
+		format: "cjs",
+		sourceMap: true
+	})
+	.pipe(source("main.js", "./temp/"))
+	.pipe(buffer())
+	.pipe(tsRollupProject())
+	.pipe(rename('NgGrid.aot.js'))
+	.pipe(gulp.dest('./dist/bundles'))
+	.pipe(uglify())
+	.pipe(rename('NgGrid.aot.min.js'))
+	.pipe(gulp.dest('./dist/bundles'));
+});
+
+gulp.task("rollup-tidy", function(done) {
+	return del(["temp"], done);
+});
+
 gulp.task('test-clean-build', function(done) {
-	return del(['test/*.js'], done)
+	return del(['test/*.js'], done);
 });
 
 gulp.task('test-build', ['test-clean-build'], function () {
@@ -102,11 +177,7 @@ gulp.task('clean', function(done) {
 });
 
 gulp.task('build', ['clean'], function() {
-	return gulp.start('libs', 'html', 'css', 'ts', 'ngc');
-});
-
-gulp.task('build-aot', ['clean'], function() {
-	return gulp.start('css', 'ngc');
+	return runSequence(['libs', 'html', 'css', 'ts', 'ngc'], ["rollup", "rollup-aot"], "rollup-tidy");
 });
 
 gulp.task('rebuild', ['clean'], function() {
