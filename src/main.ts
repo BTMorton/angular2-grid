33b4c9242d751bcdb29e7c5b76097a8ed9aacd42
Ben Morton <ben.morton@pebble.tv>
2018-07-17 20:45:09 +0100

    Expose NgConfigFixDirection, fixes #294. Update angular peer dep to include v5, for #292.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 export { NgGrid } from "./directives/NgGrid";
 export { NgGridItem } from "./directives/NgGridItem";
 export { NgGridPlaceholder } from "./components/NgGridPlaceholder";
-export { NgGridConfig, NgGridItemConfig, NgGridItemEvent, NgGridItemSize, NgGridItemPosition, NgGridRawPosition, NgGridItemDimensions } from "./interfaces/INgGrid";
+export {
+	NgGridConfig,
+	NgGridItemConfig,
+	NgGridItemEvent,
+	NgGridItemSize,
+	NgGridItemPosition,
+	NgGridRawPosition,
+	NgGridItemDimensions,
+	NgConfigFixDirection,
+} from "./interfaces/INgGrid";
 export { NgGridModule } from "./modules/NgGrid.module";
